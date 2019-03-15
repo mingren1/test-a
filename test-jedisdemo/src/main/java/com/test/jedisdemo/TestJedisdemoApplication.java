85607389b2e79629acd16eb26f9d79757d7c019a
wang <18522531324@163.com>
2019-03-15 22:26:17 +0800

    springboot整合springtask

---
+++ test-jedisdemo/src/main/java/com/test/jedisdemo/TestJedisdemoApplication.java
@@ -0,0 +1,14 @@
+package com.test.jedisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJedisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJedisdemoApplication.class, args);
+	}
+
+}
+
